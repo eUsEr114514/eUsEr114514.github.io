783b5855bf72babf3aa2a0ab118e13ac3cc66dcc
ElectronDZ75 <142735057+eUsEr114514@users.noreply.github.com>
2024-05-05 12:52:56 +0800

    Add files via upload

---
+++ homepage-main/js/home.js
@@ -0,0 +1,66 @@
+//Typed.js
+var typed = new Typed('#hi', {
+    strings: [
+        "电子Electron.",
+        "from Guangdong, China.",
+        "a student.",
+        "good at loaf on the job.",
+        "a cliper.",
+        "a bilibili up.",
+        "everything",
+        "......?"
+    ],
+    typeSpeed: 100,
+    loop: true,
+});
+
+//Nav links
+function goHome() {
+    $("#home").addClass("nav-active");
+    $("#blog").removeClass("nav-active");
+    $("#projects").removeClass("nav-active");
+    $("#homeの").show();
+    $("#blogの").hide();
+    $("#projectsの").hide();
+}
+
+function goBlog() {
+    $("#blog").addClass("nav-active");
+    $("#home").removeClass("nav-active");
+    $("#projects").removeClass("nav-active");
+    $("#blogの").show();
+    $("#homeの").hide();
+    $("#projectsの").hide();
+
+    $.get("https://blog.zhicheng233.top/rss2.xml", function (data) {
+        $('#blogs').empty();
+        $(data).find('item').each(function () {
+            var $item = $(this);
+            var title = $item.find('title').text();
+            var link = $item.find('link').text();
+            //var description = $item.find('description').text();
+            //var pubDate = $item.find('pubDate').text();
+            var html = "<div class=\"box-item\">";
+            html += "<b>" + title + "</b>";
+            //html += "<em>" + timeString(pubDate) + "</em>";
+            //html += "<p>" + description + "</p>";
+            html += "&nbsp;&nbsp;->&nbsp;&nbsp;<a href=\"" + link + "\" target=\"_blank\">Read More</a>";
+            html += "</div>";
+            
+            $('#blogs').append(html);
+        });
+    });
+}
+
+function goProjects() {
+    $("#projects").addClass("nav-active");
+    $("#blog").removeClass("nav-active");
+    $("#home").removeClass("nav-active");
+    $("#projectsの").show();
+    $("#blogの").hide();
+    $("#homeの").hide();
+}
+
+function print() {
+    alert("QQ：316951535")
+}
